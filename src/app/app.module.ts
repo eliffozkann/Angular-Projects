82236a841460caf1dc3d0a99f5efbc637c08d222
Elif Özkan <elffozkann@gmail.com>
2022-10-28 17:18:46 +0300

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { ListviewComponent } from './components/listview/listview.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    ListviewComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
